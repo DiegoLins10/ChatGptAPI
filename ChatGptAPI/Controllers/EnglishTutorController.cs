020b97dcba6d815324e94d161a152acf24dbc4a9
diegolins10 <dede140126>
2023-02-22 18:56:34 -0300

    criando controller

--- ChatGptAPI/Controllers/EnglishTutorController.cs
+++ ChatGptAPI/Controllers/EnglishTutorController.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace ChatGptAPI.Controllers
 {
+    [Controller]
+    [Route("api/english-tutor")]
     public class EnglishTutorController : ControllerBase
     {
-       
+        private readonly HttpClient _httpClient;
+
+        public EnglishTutorController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string text, [FromServices] IConfiguration configuration)
+        {
+
+        }
     }
 }
--- ChatGptAPI/Startup.cs
+++ ChatGptAPI/Startup.cs
@@ -1,3 +1,4 @@
+using ChatGptAPI.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +27,7 @@ namespace ChatGptAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddHttpClient<EnglishTutorController>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {

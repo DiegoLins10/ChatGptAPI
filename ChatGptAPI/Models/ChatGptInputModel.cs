0d5ab2aaef2f490e0bc810ff6872f931da75bf86
Diego Fernandes Lins <diegolins@itsseg.com>
2023-02-22 19:19:23 -0300

    Configurando models

--- ChatGptAPI/Models/ChatGptInputModel.cs
+++ ChatGptAPI/Models/ChatGptInputModel.cs
@@ -7,9 +7,17 @@ namespace ChatGptAPI.Models
 {
     public class ChatGptInputModel
     {
+        public ChatGptInputModel(string prompt)
+        {
+            this.prompt = $"Correct this phrase: {prompt}";
+            temperature = 0.2m;
+            max_tokens = 100;
+            model = "text-davinci-003";
+        }
+
         public string model { get; set; }
         public string prompt { get; set; }
         public int max_tokens { get; set; }
-        public int temperature { get; set; }
+        public decimal temperature { get; set; }
     }
 }

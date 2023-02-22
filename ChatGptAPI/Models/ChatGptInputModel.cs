caa7d4364bd1b813bbfc3425d704bc5874e03ce1
diegolins10 <dede140126>
2023-02-22 19:02:45 -0300

    alteracoes model

---
+++ ChatGptAPI/Models/ChatGptInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatGptAPI.Models
+{
+    public class ChatGptInputModel
+    {
+        public string model { get; set; }
+        public string prompt { get; set; }
+        public int max_tokens { get; set; }
+        public int temperature { get; set; }
+    }
+}
---
+++ ChatGptAPI/Models/ChatGptViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatGptAPI.Models
+{
+    public class ChatGptViewModel
+    {
+        public string id { get; set; }
+        public string @object { get; set; }
+        public int created { get; set; }
+        public string model { get; set; }
+        public List<Choice> choices { get; set; }
+        public Usage usage { get; set; }
+    }
+
+    public class Choice
+    {
+        public string text { get; set; }
+        public int index { get; set; }
+        public object logprobs { get; set; }
+        public string finish_reason { get; set; }
+    }
+
+
+    public class Usage
+    {
+        public int prompt_tokens { get; set; }
+        public int completion_tokens { get; set; }
+        public int total_tokens { get; set; }
+    }
+}
